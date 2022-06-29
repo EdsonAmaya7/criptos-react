e22341bd6241e8529381e03a97cabcfafbf3b674
Edson Amaya <edson_amaya99@live.com>
2022-06-28 22:05:47 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1
+`
+font-family: 'Lato',sans-serif;
+color: #FFF;
+`
+
+function App() {
+
+  return (
+   <Heading>Desde el cotizador</Heading>
+  )
+}
+
+export default App
